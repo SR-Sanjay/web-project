cd0386c299c1807d11bdceff4d893c0132c91de1
holiday <rjaicreates@gmail.com>
2025-11-17 05:20:16 +0530

    feat:finish

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-function fun(){
-    let heightInput=prompt('Enter your height')
-    let weightInput=prompt('Enter your Weight')
+function fun() {
+  let heightInput = prompt("Enter your height");
+  let weightInput = prompt("Enter your Weight");
 
-    let heightCm=parseFloat(heightInput)
-    let weight=parseFloat(weightInput)
+  let heightCm = parseFloat(heightInput);
+  let weight = parseFloat(weightInput);
 
+  let height = heightCm / 100;
 
-    let height=heightCm/100
+  let bmi = weight / (height * height);
 
-    let bmi=weight/(height*height)
+  document.querySelector(".result").innerHTML = `Your BMI is : ${bmi.toFixed(2)}`;
 
-    document.querySelector('.result').innerHTML=`Your BMI is : ${bmi.toFixed(2)}`
-
+  if (bmi >20 && bmi<=24) {
+    document.body.style.backgroundImage = "url('train.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+  }
+  else if(bmi>=25){
+    document.body.style.backgroundImage = "url('run.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+  }
+  else if(bmi<=20){
+    document.body.style.backgroundImage = "url('food.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+  }else{
+    document.body.style.backgroundImage = "url('wall.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+  }
 }
