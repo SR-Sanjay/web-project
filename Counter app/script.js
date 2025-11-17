cf5b594860877fda8bc04203ac9186c4cf5915a5
holiday <rjaicreates@gmail.com>
2025-11-17 12:09:44 +0530

    feat:New project

---
+++ Counter app/script.js
@@ -0,0 +1,14 @@
+var count=0
+function incree(){
+result=count +=1
+document.querySelector('.show').innerText=result
+}
+function decree(){
+    this.count
+    result=count-=1
+    document.querySelector('.show').innerText=result
+}
+function reset(){
+    count=0
+    document.querySelector('.show').innerText=count
+}
